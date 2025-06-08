a75570799e78e7d84644a8b4e2fa03e87a35be04
FireFox33 <122173238+FireFox33@users.noreply.github.com>
2025-06-08 22:41:21 +0530

    Coordinate Descent Algorithm - MATLAB

---
+++ 1_omp_matlab/COD_MATLAB.m
@@ -0,0 +1,54 @@
+clc; clear; close all;
+
+n = 256;          
+m = 64;           
+K = 5;            
+alpha = 0.05;      
+i = 50;      
+
+z_true = zeros(n,1);
+idx = randperm(n, K);
+z_true(idx) = randn(K,1);
+
+Psi = dct(eye(n));         
+x = Psi * z_true;   % real signal
+
+% Gaussian noise
+snr = 10; 
+s_p = norm(x)^2 / m;
+n_p = s_p / (10^(snr/10));
+noise = sqrt(n_p) * randn(m, 1);
+
+Phi = randn(m,n);
+Phi = Phi ./ vecnorm(Phi')'; 
+%y = Phi * x;  
+y = Phi * x + noise;
+
+theta = Phi * Psi;            
+
+% Coordinate Descent Algorithm
+z = zeros(n,1);
+B = theta' * y;
+S = eye(n) - theta' * theta;
+
+for t = 1:i
+    z_bar = sign(B) .* max(abs(B) - alpha, 0); 
+    [~, k] = max(abs(z - z_bar));              
+
+    delta = z_bar(k) - z(k);
+    B = B + S(:,k) * delta;
+    z(k) = z_bar(k);
+end
+
+x_rec = Psi * z;    % recovered ignal
+
+figure;
+plot(x, 'b-'); hold on;
+plot(x_rec, 'r--');
+legend('Original Signal', 'Reconstructed Signal');
+title('Original vs Reconstructed Signal');
+xlabel('Index');
+ylabel('Amplitude');
+grid on;
+
+
